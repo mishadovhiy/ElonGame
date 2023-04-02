7093906b1040cb0508459abd1ed61e4d23904949
Misha Dovhiy <hi@dovhiy.com>
2023-04-02 22:36:53 +0300

    player move: not completed

--- ElonGame/Scenes/GameScene/GameScene.swift
+++ ElonGame/Scenes/GameScene/GameScene.swift
@@ -18,10 +18,17 @@ class GameScene: SKScene {
     
     var knobRadius:CGFloat = 50 
     
+    var previusTimeInterval:TimeInterval = 0
+    var playerIsFacingRight = true
+    let playerSpeed:Double = 4
+    
+    
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
         self.declareUI()
+        print(self.view?.frame.width, " rgtefrdwfvrgt")
+
     }
     
     func declareUI() {
@@ -43,5 +50,43 @@ extension GameScene {
         moveBack.timingMode = .linear
         joysticKnob?.run(moveBack)
         joysticAction = false
+    }
+}
+
+
+extension GameScene {
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+        let deltaTime:Double = 1//currentTime - previusTimeInterval
+        previusTimeInterval = deltaTime
+        print(deltaTime, " hntgrfedxcfvg")
+        guard let knob = joysticKnob else { return }
+        
+        let xPosition = Double(knob.position.x)
+        let dx = deltaTime * xPosition * playerSpeed
+        let displeisment = CGVector(dx: dx, dy: 0)
+        if displeisment.dx != 0 {
+            print(displeisment.dx, "grfedws")
+        }
+        let move = SKAction.move(by: displeisment, duration: 1)
+        //player?.run(move)
+        
+        
+        let faceAction:SKAction!
+        let movingRight = xPosition > 0
+        let movingLeft = xPosition < 0
+        if movingLeft && playerIsFacingRight {
+            playerIsFacingRight = false
+            
+            let faceMovement = SKAction.scaleX(to: -1, duration: 0)
+            faceAction = SKAction.sequence([move, faceMovement])
+        } else if movingRight && !playerIsFacingRight {
+             playerIsFacingRight = true
+            let faceMovement = SKAction.scaleX(to: 1, duration: 0)
+            faceAction = SKAction.sequence([move, faceMovement])
+        } else {
+            faceAction = move
+        }
+        player?.run(faceAction)
     }
 }
